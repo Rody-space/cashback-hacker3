2994dbbc4f8aac2c8e6af93536247853c5e3ba1b
Rody-space <rodion.bastrigin@gmail.com>
2026-01-25 19:39:03 +0700

    "testng"

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
@@ -0,0 +1,21 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTestNG {
+
+    @Test
+    public void testRemain_BelowBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int result = service.remain(900);
+        assertEquals(result, 100, "Should return 100 for 900 rubles");
+    }
+
+    @Test
+    public void testRemain_ExactBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int result = service.remain(1000);
+        assertEquals(result, 0, "Should return 0 for exact 1000 rubles");
+    }
+}
